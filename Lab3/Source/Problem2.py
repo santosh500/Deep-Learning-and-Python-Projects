6adfbb79ac84dcb556a94b1789f90dcc95041d85
Paul Charles <santosh41690@gmail.com>
2018-03-06 20:53:32 -0600

    changes

--- Lab3/Source/Problem2.py
+++ Lab3/Source/Problem2.py
@@ -23,14 +23,13 @@ print(len(Features_test))
 
 X = np.array([[-1, -1], [-2, -1], [1, 1], [2, 1]])
 y = np.array([1, 1, 2, 2])
+#This code is referenced from: http://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html
 linear = SVC()
 linear.fit(Features_train, Labels_train)
 SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
     decision_function_shape='ovr', degree=3, gamma='auto', kernel='linear',
     max_iter=-1, probability=False, random_state=None, shrinking=True,
     tol=0.001, verbose=False)
-for i in Features_test:
-    print(linear.predict([[4.8, 3.4, 1.6, 0.2]]))
 #predict values
 linearArray =[]
 linearCount = 0
@@ -42,15 +41,13 @@ for i in range(0, len(Features_test)):
     if((linear.predict([Features_test[i]])) != (Labels_test[i])):
         linearCount += 1
 
-
+#This code is referenced from: http://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html
 rbf = SVC()
 rbf.fit(Features_train, Labels_train)
 SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
     decision_function_shape='ovr', degree=3, gamma='auto', kernel='rbf',
     max_iter=-1, probability=False, random_state=None, shrinking=True,
     tol=0.001, verbose=False)
-for i in Features_test:
-    print(rbf.predict([[4.8, 3.4, 1.6, 0.2]]))
 
 #predict values
 rbfArray =[]
