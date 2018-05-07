64d1e9473a2b904acb3f597c938d14813ebc50c6
Paul Charles <santosh41690@gmail.com>
2018-05-07 18:39:37 -0500

    Commit ICP7

---
+++ DL_ICP7/SourceCode/MountainCarNoDQN.py
@@ -0,0 +1,10 @@
+#This was referenced from: https://gym.openai.com/docs/
+import gym
+space = gym.make('MountainCar-v0')
+for i in range(50):
+    sample = space.reset()
+    for t in range(100):
+        space.render()
+        print(sample)
+        action = space.action_space.sample()
+        sample, reward, done, info = space.step(action)
